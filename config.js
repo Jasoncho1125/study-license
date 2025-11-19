af7b72f85e73dce0567a1c337cc9fc760371360f
Jason Cho <choin337@gmail.com>
2025-11-19 12:35:07 +0900

    전기기사 필기 -전자기학 추가

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // 🎯 사용자 지정 변수 (공유 설정)
 export const APP_NAME = "자격증 공부";
-export const APP_VERSION = "v0.1";
-export const JSON_FILE_NAME = "study_data-v0.1.json"; 
+export const APP_VERSION = "v0.3";
+export const JSON_FILE_NAME = "study_data-v0.2.json"; 
 export const IMAGE_BASE_PATH = "/image/"; 
